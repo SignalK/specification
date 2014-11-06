f402a7ce648e88a549a274e775eeb4a18c97b324
Teppo Kurki <teppo.kurki@iki.fi>
2014-11-05 20:15:06 -0500

    Add delta validation and a test for it

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ function chaiAsPromised(chai, utils) {
       , 'expected #{this} to not be valid SignalK'
       );
   });  
+  Assertion.addProperty('validSignalKDelta', function () {
+    var result = validateDelta(this._obj);
+    var message = result.errors.length === 0 ? '' : result.errors[0].message + ':' + result.errors[0].dataPath + 
+      ' (' + (result.errors.length-1) + ' other errors not reported here)';
+    this.assert(
+      result.valid
+      , message
+      , 'expected #{this} to not be valid SignalK delta'
+      );
+  });
 }
 
 function validate(tree) {
@@ -37,5 +47,19 @@ function validate(tree) {
   return valid;
 }
 
+function validateDelta(delta, ignoreContext) {
+  var tv4 = require('tv4');
+  var deltaSchema = require('./schemas/delta.json');
+  var definitions = require('./schemas/definitions.json');
+  tv4.addSchema('https://signalk.github.io/specification/schemas/definitions.json', definitions);
+
+  if (ignoreContext) {
+    delta.context = 'ignored the context, so place a placeholder there';
+  }
+  var valid = tv4.validateMultiple(delta, deltaSchema, true, true);
+  return valid;
+}
+
 module.exports.validate = validate;
+module.exports.validateDelta = validateDelta;
 module.exports.chaiModule = chaiAsPromised;
