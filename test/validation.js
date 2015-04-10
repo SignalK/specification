602afe9cb2d1a6c957661a8275fb96bf0393878a
Teppo Kurki <teppo.kurki@iki.fi>
2015-04-10 20:16:16 +0300

    Reformat, change comment

--- test/validation.js
+++ test/validation.js
@@ -4,12 +4,11 @@ chai.use(require('../index.js').chaiModule);
 
 
 
-describe('Delta validation', function () {
-  it('works', function () {
+describe('Delta validation', function() {
+  it('Simple path with value is valid and not when modified', function() {
     var msg = {
-      context:'bar',
-      updates:[
-      {
+      context: 'bar',
+      updates: [{
         "source": {
           "label": "",
           "type": "NMEA2000",
@@ -17,14 +16,11 @@ describe('Delta validation', function () {
           "src": "204"
         },
         "timestamp": "2013-10-08T15:47:28.263Z",
-        "values": [
-          { 
-            "path":"123",
-            "value":1234
-          }
-        ]
-      }
-      ]
+        "values": [{
+          "path": "123",
+          "value": 1234
+        }]
+      }]
     };
     msg.should.be.validSignalKDelta;
     msg.foo = 'bar';
