4e1543198f47d9cc45932553a69e3a070685799c
Teppo Kurki <teppo.kurki@iki.fi>
2015-11-23 21:32:21 +0200

    Add extract script & metadata Add js script to extract metadata from the schema files as well as the extracted metadata

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function getTv4() {
-  var tv4 = require('tv4');
-  var vesselSchema = require('./schemas/vessel.json');
-  tv4.addSchema('https://signalk.github.io/specification/schemas/vessel.json', vesselSchema);
-  var definitions = require('./schemas/definitions.json');
-  tv4.addSchema('https://signalk.github.io/specification/schemas/definitions.json', definitions);
-
   var subSchemas = {
     'alarms': require('./schemas/groups/alarms.json'),
     'communication': require('./schemas/groups/communication.json'),
@@ -19,6 +12,15 @@ function getTv4() {
     'steering': require('./schemas/groups/steering.json'),
     'tanks': require('./schemas/groups/tanks.json')
   };
+
+
+function getTv4() {
+  var tv4 = require('tv4');
+  var vesselSchema = require('./schemas/vessel.json');
+  tv4.addSchema('https://signalk.github.io/specification/schemas/vessel.json', vesselSchema);
+  var definitions = require('./schemas/definitions.json');
+  tv4.addSchema('https://signalk.github.io/specification/schemas/definitions.json', definitions);
+
   for (var schema in subSchemas) {
     tv4.addSchema('https://signalk.github.io/specification/schemas/groups/' + schema + '.json', subSchemas[schema]);
   }
@@ -136,3 +138,4 @@ module.exports.validateDelta = validateDelta;
 module.exports.chaiModule = chaiAsPromised;
 module.exports.i18n = require('./i18n/');
 module.exports.getTv4 = getTv4;
+module.exports.subSchemas = subSchemas;
