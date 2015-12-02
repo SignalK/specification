bc69594ba9e2bd34f323e022ebf5014340d26769
Teppo Kurki <teppo.kurki@iki.fi>
2015-12-02 23:29:26 +0200

    Rename

--- test/propulsion.js
+++ test/propulsion.js
@@ -4,6 +4,6 @@ chai.use(require('../index.js').chaiModule);
 
 describe('Propulsion in the full tree', function() {
   it("Sample full tree is valid", function() {
-    require('./data/engine.json').should.be.validSignalKVessel;
+    require('./data/propulsion.json').should.be.validSignalKVessel;
   });
 });
