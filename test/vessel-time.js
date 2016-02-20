4579a2abfb0207d0ea3a844dd8076ce583db2550
Teppo Kurki <teppo.kurki@iki.fi>
2016-02-20 10:01:21 +0200

    Fix comment

--- test/vessel-time.js
+++ test/vessel-time.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 chai.Should();
 chai.use(require('../index.js').chaiModule);
 
-describe('Propulsion in the full tree', function() {
+describe('Time in the full tree', function() {
   it("Sample full tree is valid", function() {
     require('../samples/vessel-time.json').should.be.validSignalK;
   });
