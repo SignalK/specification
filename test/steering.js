13708db64250dd8619c129610d7f9309bea831e8
joabakk <joachim@Arnhilds-MacBook-Pro.local>
2016-09-17 12:55:31 +0200

    Fix for test

--- test/steering.js
+++ test/steering.js
@@ -2,7 +2,7 @@ var chai = require('chai');
 chai.Should();
 chai.use(require('../index.js').chaiModule);
 
-describe('Tanks in the full tree', function() {
+describe('Steering in the full tree', function() {
   it("Sample full tree is valid", function() {
     require('./data/steering.json').should.be.validSignalKVessel;
   });
