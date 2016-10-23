af23e3ff47af5e1a23c5ce8527c5aefb0c15bb24
Antti Risteli <antti@kiinnost.us>
2016-10-23 18:35:22 +0300

    Add test for missing reference keys

--- test/schemaReferences.js
+++ test/schemaReferences.js
@@ -1,6 +1,8 @@
 var assert = require('chai').assert
 
 var signalk = require('../');
+var RefParser = require('json-schema-ref-parser');
+var path = require('path');
 
 describe('Schema references', function() {
   it('missing files are not referenced', function() {
@@ -9,5 +11,15 @@ describe('Schema references', function() {
 
     tv4.validate({}, signalkSchema);
     assert.lengthOf(tv4.getMissingUris(), 0, 'There should be no missing schema uris, but found ' + tv4.getMissingUris());
+  })
+
+  it('all references are valid', function(done) {
+    RefParser.dereference(path.join(__dirname, '../schemas/signalk.json'), function(err, schema) {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
   })
 });
