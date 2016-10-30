1e07e3a1b531c2dc7c57a7aabcd6ad563e334a71
Antti Risteli <joux3@users.noreply.github.com>
2016-10-30 18:17:53 +0200

    Validate missing schema references & fix missing references (#278)
    * Properly name schema reference test
    * Add test for missing reference keys
    * Refer to definitions/sourceRef instead of definitions/$source
    * Refer explicitly to definitions.json#/definitions/timestamp
    * Refer to lineString and polygon defined in geometry.json

--- test/schemaReferences.js
+++ test/schemaReferences.js
@@ -1,13 +1,25 @@
 var assert = require('chai').assert
 
 var signalk = require('../');
+var RefParser = require('json-schema-ref-parser');
+var path = require('path');
 
 describe('Schema references', function() {
-  it('all references are valid', function() {
+  it('missing files are not referenced', function() {
     var signalkSchema = require('../schemas/signalk.json');
     var tv4 = signalk.getTv4();
 
     tv4.validate({}, signalkSchema);
     assert.lengthOf(tv4.getMissingUris(), 0, 'There should be no missing schema uris, but found ' + tv4.getMissingUris());
+  })
+
+  it('all references are valid', function(done) {
+    RefParser.dereference(path.join(__dirname, '../schemas/signalk.json'), function(err, schema) {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
   })
 });
