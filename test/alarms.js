ebe4ae331f1e9fc7a10ac966f81cc9c8b9b6f814
bkp7 <barry@x-ware.co.uk>
2017-01-23 20:14:08 +0000

    Additional tests for notifications
    Several added covering a number of different issues

--- test/alarms.js
+++ test/alarms.js
@@ -10,11 +10,52 @@ describe('Notifications in the full tree', function() {
   it("should be valid for deep keys", function() {
     require('./data/alarms-deep.json').should.be.validSignalK;
   });
-  //must use enum
-  it("should use alarmState enum", function() {
+  //or shallow embedded
+  it("should be valid for shallow keys", function() {
+    require('./data/alarms-shallow.json').should.be.validSignalK;
+  });
+  //and work with multiple alarms at multiple levels
+  it("should be valid with multiple alarms at multiple levels", function() {
+    require('./data/alarms-comprehensive.json').should.be.validSignalK;
+  });
+  //they should allow branches with names similar to mob etc
+  it("they should allow branches with names similar to mob etc", function() {
+    require('./data/alarms-allowablenames.json').should.be.validSignalK;
+  });
+  //must use alarmState enum
+  it("must use alarmState enum", function() {
     require('./data/alarms-invalid1.json').should.not.be.validSignalK;
   });
-  it("should use alarmMethod array with enum", function() {
+  //must use alarmMethod enum
+  it("must use alarmMethod array with enum", function() {
     require('./data/alarms-invalid2.json').should.not.be.validSignalK;
+  });
+  //must not have sub trees beneath well-defined notifications
+  it("must not have sub trees beneath well-defined notifications", function() {
+    require('./data/alarms-invalid3.json').should.not.be.validSignalK;
+  });
+  //must not have dot notation in branch names
+  it("must not have dot notation in branch names", function() {
+    require('./data/alarms-invalid4.json').should.not.be.validSignalK;
+  });
+  //must not have have spaces in branch names
+  it("must not have have spaces in branch names", function() {
+    require('./data/alarms-invalid5.json').should.not.be.validSignalK;
+  });
+  //must not have incorrectly spelled keys
+  it("must not have incorrectly spelled keys", function() {
+    require('./data/alarms-invalid6.json').should.not.be.validSignalK;
+  });
+  //must not use well-defined notifications at deep levels
+  it("must not use well-defined notifications at deep levels", function() {
+    require('./data/alarms-invalid7.json').should.not.be.validSignalK;
+  });
+  //must not have additional keys in notifications
+  it("must not have additional keys in notifications", function() {
+    require('./data/alarms-invalid8.json').should.not.be.validSignalK;
+  });
+  //must use array of states not string
+  it("must use array of states not string", function() {
+    require('./data/alarms-invalid9.json').should.not.be.validSignalK;
   });
 });
