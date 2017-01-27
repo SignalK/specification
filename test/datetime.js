d7ead492aea8de2edc3b62881c8615167ad4168a
bkp7 <barry@x-ware.co.uk>
2017-01-27 20:29:58 +0000

    Added timezoneRegion to environment.json
    This allows the timezone to be defined as a region and thus allow for daylight saving as is commonplace in most systems. Also added tests for the new keys.

--- test/datetime.js
+++ test/datetime.js
@@ -5,5 +5,16 @@ chai.use(require('../index.js').chaiModule);
 describe('Datetime in the full tree', function() {
   it("should be valid", function() {
     require('./data/datetime.json').should.be.validSignalK;
+  })
+});
+describe('Timezone in Environment', function() {
+  it("should be valid", function() {
+    require('./data/datetime_timezone.json').should.be.validSignalK;
+  });
+  it("should fail with invalid timezoneRegion", function() {
+    require('./data/datetime_timezone-invalid1.json').should.not.be.validSignalK;
+  });
+  it("should fail with timezoneRegion but no timezone", function() {
+    require('./data/datetime_timezone-invalid2.json').should.not.be.validSignalK;
   });
 });
