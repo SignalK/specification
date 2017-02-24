faa5fcf2e26bb727d6b877cd179a95b1cda85f8c
rob42 <robert@42.co.nz>
2017-02-24 15:31:24 +1300

    Add aisType to sources

---
+++ test/ais.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+chai.Should();
+chai.use(require('../index.js').chaiModule);
+
+describe('Valid Ais in the full tree', function() {
+  it("Sample full tree is valid", function() {
+    require('./data/ais-source.json').should.be.validSignalK;
+  });
+});
+
+describe('Invalid ais source in the full tree', function() {
+  it("Sample full tree is valid", function() {
+    require('./data/ais-invalid-source0.json').should.not.be.validSignalK;
+  });
+});
+describe('Invalid ais source in the full tree', function() {
+  it("Sample full tree is valid", function() {
+    require('./data/ais-invalid-source28.json').should.not.be.validSignalK;
+  });
+});
