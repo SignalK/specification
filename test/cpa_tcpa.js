cc4d3f90e1e1cb1e74286968f89e8523f7fa453c
Teppo Kurki <teppo.kurki@iki.fi>
2017-10-22 22:39:31 +0300

    fix: cpa tests

--- test/cpa_tcpa.js
+++ test/cpa_tcpa.js
@@ -1,9 +1,9 @@
 var chai = require('chai');
 chai.Should();
-chai.use(require('../index.js').chaiModule);
+chai.use(require('../dist/index.js').chaiModule);
 
 describe('Valid cpa and tcpa values in the full tree', function() {
   it("Sample full tree is valid", function() {
-    require('./data/ais-cpa-tcpa.json').should.be.validSignalK;
+    require('./data/ais-cpa-tcpa.json').should.be.validSignalKIgnoringSelf;
   });
 });
