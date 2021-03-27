9f601fd383cd9c2f76e3d20108d85e2a714e5565
Teppo Kurki <teppo.kurki@reaktor.fi>
2021-03-27 09:50:47 +0200

    1.6.0

--- src/index.js
+++ src/index.js
@@ -39,23 +39,23 @@ var FullSignalK = require('./fullsignalk');
 function getTv4() {
   var tv4 = require('tv4');
   var vesselSchema = require('../schemas/vessel.json');
-  tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/vessel.json', vesselSchema);
+  tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/vessel.json', vesselSchema);
   var aircraftSchema = require('../schemas/aircraft.json');
-  tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/aircraft.json', aircraftSchema);
+  tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/aircraft.json', aircraftSchema);
   var atonSchema = require('../schemas/aton.json');
-  tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/aton.json', atonSchema);
+  tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/aton.json', atonSchema);
   var sarSchema = require('../schemas/sar.json');
-  tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/sar.json', sarSchema);
+  tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/sar.json', sarSchema);
   var definitions = require('../schemas/definitions.json');
-  tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/definitions.json', definitions);
+  tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/definitions.json', definitions);
 
   for (var schema in subSchemas) {
-    tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/groups/' + schema + '.json', subSchemas[schema]);
+    tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/groups/' + schema + '.json', subSchemas[schema]);
   }
 
   // HACK! two different IDs should not point to the same schema
   var externalGeometry = require('../schemas/external/geojson/geometry.json');
-  tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/external/geojson/geometry.json', externalGeometry);
+  tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/external/geojson/geometry.json', externalGeometry);
   tv4.addSchema('http://json-schema.org/geojson/geometry.json', externalGeometry);
 
   tv4.addFormat(require('tv4-formats'))
@@ -79,7 +79,7 @@ function validateDelta(delta, ignoreContext) {
   var tv4 = require('tv4');
   var deltaSchema = require('../schemas/delta.json');
   var definitions = require('../schemas/definitions.json');
-  tv4.addSchema('https://signalk.org/specification/1.5.1/schemas/definitions.json', definitions);
+  tv4.addSchema('https://signalk.org/specification/1.6.0/schemas/definitions.json', definitions);
 
   if (ignoreContext) {
     delta.context = 'ignored the context, so place a placeholder there';
